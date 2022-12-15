f1c84deb92ed97c246ca7b00c4dc2dcb3cd70755
Gabriel <gabrc52@gmail.com>
2022-12-15 01:28:21 -0500

    turn html into php

---
+++ verify.php
@@ -0,0 +1,32 @@
+<?php
+
+/// Hardcoded for now
+$server = '1049418013323055124';
+$role = '1050870099667603466';
+
+/// TODO: add code
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="verify.css">
+</head>
+<body>
+    <div id="main">
+        <h1>One more thing!</h1>
+        <p>Is the name we have on file correct?</p>
+        <h2 id="usr_name">Tim A Beaver</h2> <!--TODO: make dynamic-->
+        <div id="buttons">
+            <a class="button" href="" id="btn_yes">Yes!</a></li> <!--TODO: add link-->
+            <a class="button" href="" id="btn_no">No, let me correct it</a> <!--TODO: add link-->
+        </div>
+    </div>
+</body>
+</html>
+
+
