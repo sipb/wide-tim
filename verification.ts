1d1c387a0ede159f5350d82f8bf37cf00a4768fd
GenericP3rson <genericp3rson@gmail.com>
2022-12-16 18:14:12 -0500

    initial (kind of clunky) verification command

--- verification.ts
+++ verification.ts
@@ -1,4 +1,4 @@
-import { Client, Events, GuildMember } from 'discord.js';
+import { Client, Events, GuildMember, Message } from 'discord.js';
 import { readFileSync } from 'fs';
 import { sha256 } from 'js-sha256';
 
@@ -18,6 +18,20 @@ In order to prove you are an adMITted member of the Class of 2027, and in need o
 Once you have done that, I will then use all my considerable strength to yeet you into the server with the other adMITs where you belong. If, for some reason, you have continued trouble gaining access to the server, send an email to 2027discordadmin@mit.edu for assistance.`);
         } catch (err) {
             console.log(`Could not send verification DM to ${member.user.tag}: ${err}`);
+        }
+    });
+
+    client.on(Events.MessageCreate, async (message: Message) => {
+        if (message.content == '/verify') {
+            try {
+                await message.member?.send(`Hi! I am Wide Tim, the most massive member of the 2027 Discord. I use my large arms to hold off people who shouldn't be here and to hug people who should, and I have lumbered into your DMs to help with that.
+
+In order to prove you are an adMITted member of the Class of 2027, and in need of hugs, please go to ${get2027VerifyLink(message.member?.id)} to verify your account using your email address. 
+
+Once you have done that, I will then use all my considerable strength to yeet you into the server with the other adMITs where you belong. If, for some reason, you have continued trouble gaining access to the server, send an email to 2027discordadmin@mit.edu for assistance.`);
+            } catch (err) {
+                console.log(`Could not send verification DM to ${message.member?.user.tag}: ${err}`);
+            }
         }
     });
 }
