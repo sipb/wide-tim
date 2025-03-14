ed89943e3de965ebf49ba2ae13ab0a1a614dd8fb
Gabriel Rodríguez <rgabriel@mit.edu>
2025-03-14 18:24:01 -0400

    remove irrelevant parts of welcome message

--- config.ts
+++ config.ts
@@ -7,11 +7,5 @@ export = {
     
     guild_2029: '1350137441062883469',
     admitted_role_2029: '1350137545362767882',
-    post_verification_dm: `Hooray! You have been successfully verified, and I love your name. You should now be able to see a bunch of channels. Say hello! 
-
-If you're new to Discord, or just want some help finding your way around, we wrote these for you: 
-    
-* A Hitchiker's Guide to the 2029 Discord: https://docs.google.com/document/d/1yMXl3mM3SqqYibTrsT8yoE4DlJFtGWVMb6h8wOrQ9j0/edit#
-    
-* A reference spreadsheet of channels, roles, and bots: https://docs.google.com/spreadsheets/d/1rYldgitCVmVTYllFFgu4w1GdE_E7lgJKPeJz365XRd0/edit#gid=868164425`,
+    post_verification_dm: `Hooray! You have been successfully verified, and I love your name. You should now be able to see a bunch of channels. Say hello!`,
 }
